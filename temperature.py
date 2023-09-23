2e4a36192d417ac6ae7b46de448519c50c045ac0
Ras <rasrahim823@gmail.com>
2023-09-22 22:09:30 -0400

    1st submission

--- temperature.py
+++ temperature.py
@@ -21,17 +21,20 @@
 from typing import List
 
 def calculate_average_temperature(temperatures: List[float]) -> float:
+    num_temp = len(temperatures)
+    if num_temp == 0:
+        return None 
      # 1.1 TODO: # Initialize an accumulator variable to keep track of the sum of temperatures.
-   
-    
+    # temperatures = [20.5, 22.0, 18.5, 25.5, 26.0, 23.5, 19.0]   
+    temper = 0
     # 1.2 TODO:# Iterate through the `temperatures` list, updating the accumulator with the current temperature.
-
-    
+    for temp in temperatures:
+        temper += temp
+    # print(temper)
+    # calculate_average_temperature()    
     # 1.3 TODO: # Calculate and return the average temperature using the accumulated sum and the total number of readings.
+    avg_temp = temper / num_temp
+    # # 1.3 TODO: return the average temperature
+    return avg_temp
 
-
-    
-    # 1.3 TODO: return the average temperature
-    
-
-#calculate_average_temperature()
+calculate_average_temperature(temperatures = [20.5, 22.0, 18.5, 25.5, 26.0, 23.5, 19.0])
